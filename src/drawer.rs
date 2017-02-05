a17ec84c2005d72cb6b116a0d6edf45d9a09382a
Ivan Komarov <Ivan.Komarov@dfyz.info>
2017-02-05 06:17:47 +0300

    draw something EVEN MORE AWESOME!

--- src/drawer.rs
+++ src/drawer.rs
@@ -22,14 +22,23 @@ pub fn draw_tile<'a>(entities: &OsmEntities<'a>, tile: &Tile) -> Result<Vec<u8>>
 
         let cr = cs::cairo_create(s);
 
-        let translate_coord = |c, tile_c| (c - TILE_SIZE * tile_c) as f64;
-        for n in entities.nodes.iter() {
-            let coords = (n.lat(), n.lon());
-            let (x, y) = coords_to_xy(&coords, tile.zoom);
-            let real_x = translate_coord(x, tile.x);
-            let real_y = translate_coord(y, tile.y);
+        for w in entities.ways.iter() {
+            if w.node_count() == 0 {
+                continue;
+            }
+            cs::cairo_set_source_rgb(cr, 0.0, 4.0, 0.0);
+            let (x, y) = to_cairo_xy(&w.get_node(0), tile);
+            cs::cairo_move_to(cr, x, y);
+            for i in 1..w.node_count() {
+                let (x, y) = to_cairo_xy(&w.get_node(i), tile);
+                cs::cairo_line_to(cr, x, y);
+            }
+            cs::cairo_stroke(cr);
+        }
 
-            cs::cairo_arc(cr, real_x, real_y, 4.0, 0.0, 2.0*PI);
+        for n in entities.nodes.iter() {
+            let (x, y) = to_cairo_xy(n, tile);
+            cs::cairo_arc(cr, x, y, 1.0, 0.0, 2.0*PI);
             cs::cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
             cs::cairo_fill(cr);
         }
@@ -41,4 +50,13 @@ pub fn draw_tile<'a>(entities: &OsmEntities<'a>, tile: &Tile) -> Result<Vec<u8>>
     }
 
     Ok(data)
+}
+
+fn to_cairo_xy(entity: &Coords, tile: &Tile) -> (f64, f64) {
+    let coords = (entity.lat(), entity.lon());
+    let (x, y) = coords_to_xy(&coords, tile.zoom);
+    let translate_coord = |c, tile_c| (c - TILE_SIZE * tile_c) as f64;
+    let real_x = translate_coord(x, tile.x);
+    let real_y = translate_coord(y, tile.y);
+    (real_x, real_y)
 }
