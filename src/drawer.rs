6ce9bea95907c0736c782131798fbe569a3085a3
Ivan Komarov <Ivan.Komarov@dfyz.info>
2017-02-05 20:17:03 +0300

    use cairo matrices instead of manual translation

--- src/drawer.rs
+++ src/drawer.rs
@@ -1,12 +1,11 @@
 use errors::*;
 
-use coords::Coords;
 use cs;
 use geodata::reader::OsmEntities;
 use libc;
 use std::f64::consts::PI;
 use std::slice;
-use tile::{coords_to_xy, Tile, TILE_SIZE};
+use tile::{coords_to_float_xy, Tile, TILE_SIZE};
 
 unsafe extern "C" fn write_func(closure: *mut libc::c_void, data: *mut u8, len: libc::c_uint) -> cs::enums::Status {
     let png_bytes: &mut Vec<u8> = &mut *(closure as *mut Vec<u8>);
@@ -22,22 +21,25 @@ pub fn draw_tile<'a>(entities: &OsmEntities<'a>, tile: &Tile) -> Result<Vec<u8>>
 
         let cr = cs::cairo_create(s);
 
+        let get_delta = |c| -((TILE_SIZE as f64) * (c as f64));
+        cs::cairo_translate(cr, get_delta(tile.x), get_delta(tile.y));
+
         for w in entities.ways.iter() {
             if w.node_count() == 0 {
                 continue;
             }
             cs::cairo_set_source_rgb(cr, 0.0, 4.0, 0.0);
-            let (x, y) = to_cairo_xy(&w.get_node(0), tile);
+            let (x, y) = coords_to_float_xy(&w.get_node(0), tile.zoom);
             cs::cairo_move_to(cr, x, y);
             for i in 1..w.node_count() {
-                let (x, y) = to_cairo_xy(&w.get_node(i), tile);
+                let (x, y) = coords_to_float_xy(&w.get_node(i), tile.zoom);
                 cs::cairo_line_to(cr, x, y);
             }
             cs::cairo_stroke(cr);
         }
 
         for n in entities.nodes.iter() {
-            let (x, y) = to_cairo_xy(n, tile);
+            let (x, y) = coords_to_float_xy(n, tile.zoom);
             cs::cairo_arc(cr, x, y, 1.0, 0.0, 2.0*PI);
             cs::cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
             cs::cairo_fill(cr);
@@ -50,13 +52,4 @@ pub fn draw_tile<'a>(entities: &OsmEntities<'a>, tile: &Tile) -> Result<Vec<u8>>
     }
 
     Ok(data)
-}
-
-fn to_cairo_xy(entity: &Coords, tile: &Tile) -> (f64, f64) {
-    let coords = (entity.lat(), entity.lon());
-    let (x, y) = coords_to_xy(&coords, tile.zoom);
-    let translate_coord = |c, tile_c| (c as f64 - (TILE_SIZE * tile_c) as f64);
-    let real_x = translate_coord(x, tile.x);
-    let real_y = translate_coord(y, tile.y);
-    (real_x, real_y)
 }
