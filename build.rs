58f101c710e0ee2013ccdbe4c8a08c4b3f783bf7
Ivan Komarov <Ivan.Komarov@dfyz.info>
2017-02-05 05:42:22 +0300

    link with needed libraries on Win32

--- build.rs
+++ build.rs
@@ -3,6 +3,12 @@ extern crate capnpc;
 use capnpc::CompilerCommand;
 
 fn main() {
+	if ::std::env::var("TARGET").unwrap().contains("windows") {
+		for extra_lib in &["gdi32", "user32"] {
+			println!("cargo:rustc-link-lib=dylib={}", extra_lib);
+		}
+	}
+
 	CompilerCommand::new()
 		.src_prefix("schema")
 		.file("schema/geodata.capnp")
