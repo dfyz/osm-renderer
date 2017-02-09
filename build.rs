faa880fbbd55554766445b3f5e9e458594300736
Ivan Komarov <Ivan.Komarov@dfyz.info>
2017-02-09 22:48:59 +0300

    link with libraries from /usr/local/lib on Mac OS X to find cairo (also, tabs -> spaces)

--- build.rs
+++ build.rs
@@ -3,15 +3,20 @@ extern crate capnpc;
 use capnpc::CompilerCommand;
 
 fn main() {
-	if ::std::env::var("TARGET").unwrap().contains("windows") {
-		for extra_lib in &["gdi32", "user32"] {
-			println!("cargo:rustc-link-lib=dylib={}", extra_lib);
-		}
-	}
+    let target = ::std::env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        for extra_lib in &["gdi32", "user32"] {
+            println!("cargo:rustc-link-lib=dylib={}", extra_lib);
+        }
+    }
 
-	CompilerCommand::new()
-		.src_prefix("schema")
-		.file("schema/geodata.capnp")
-		.run()
-		.expect("Failed to compile Cap'N'Proto files");
+    if target.contains("darwin") {
+        println!("cargo:rustc-link-search=native={}", "/usr/local/lib");
+    }
+
+    CompilerCommand::new()
+        .src_prefix("schema")
+        .file("schema/geodata.capnp")
+        .run()
+        .expect("Failed to compile Cap'N'Proto files");
 }
