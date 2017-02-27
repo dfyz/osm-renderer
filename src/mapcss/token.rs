fda651865714ad1043a3845c6e75351c29f9445e
Ivan Komarov <Ivan.Komarov@dfyz.info>
2017-02-27 05:54:45 +0300

    More work on tokenizer (tests still fail)

--- src/mapcss/token.rs
+++ src/mapcss/token.rs
@@ -1,5 +1,3 @@
-use std::str::CharIndices;
-
 pub type ZoomLevel = Option<u8>;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -8,7 +6,7 @@ pub struct ZoomLevels {
     pub max_zoom: ZoomLevel,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Tok<'a> {
     Identifier(&'a str),
     String(&'a str),
@@ -21,8 +19,6 @@ pub enum Tok<'a> {
     RightBracket,
     LeftBrace,
     RightBrace,
-    LeftParens,
-    RightParens,
 
     Equal,
     NotEqual,
@@ -34,30 +30,90 @@ pub enum Tok<'a> {
 
     Bang,
     Dot,
-    Hyphen,
     Colon,
     DoubleColon,
     SemiColon,
 }
 
+// Grouped by the first symbol, sorted by decreasing length in each group
+// to make sure we always capture the longest token.
+const SIMPLE_TOKEN_MATCH_TABLE: &'static [(&'static str, Tok<'static>)] = &[
+    // The unambigous tokens form the first group.
+    ("[", Tok::LeftBracket),
+    ("]", Tok::RightBracket),
+    ("{", Tok::LeftBrace),
+    ("}", Tok::RightBrace),
+    (".", Tok::Dot),
+    (";", Tok::SemiColon),
+
+    ("=~", Tok::RegexMatch),
+    ("=", Tok::Equal),
+
+    ("!=", Tok::NotEqual),
+    ("!", Tok::Bang),
+
+    ("<=", Tok::LessOrEqual),
+    ("<", Tok::Less),
+
+    (">=", Tok::GreaterOrEqual),
+    (">", Tok::Greater),
+
+    ("::", Tok::DoubleColon),
+    (":", Tok::Colon),
+];
+
+type TokenResult<'a> = Result<(usize, Tok<'a>, usize), String>;
+
 pub struct Tokenizer<'a> {
     text: &'a str,
-    chars: CharIndices<'a>,
+    chars: Vec<(usize, char)>,
+    char_index: usize,
 }
 
 impl<'a> Tokenizer<'a> {
     pub fn new(input: &'a str) -> Tokenizer<'a> {
-        Tokenizer{
+        Tokenizer {
             text: input,
-            chars: input.char_indices(),
+            chars: input.char_indices().collect(),
+            char_index: 0,
         }
+    }
+
+    fn consume(&mut self, token: Tok<'a>, char_count: usize) -> TokenResult<'a> {
+        let result = (self.char_index, token, self.char_index + char_count);
+        self.char_index += char_count;
+        Ok(result)
+    }
+
+    fn get_current_char(&self) -> char {
+        self.chars[self.char_index].1
     }
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
-    type Item = (usize, Tok<'a>, usize);
+    type Item = TokenResult<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        if let None = self.chars.get(self.char_index) {
+            return None;
+        }
+
+        for &(ref simple_token, ref token_type) in SIMPLE_TOKEN_MATCH_TABLE {
+            let has_match = simple_token
+                .chars()
+                .enumerate()
+                .all(|(offset, ch)| {
+                    let text_index = self.char_index + offset;
+                    match self.chars.get(text_index) {
+                        Some(&(_, text_ch)) if ch == text_ch => true,
+                        _ => false,
+                    }
+                });
+            if has_match {
+                return Some(self.consume(*token_type, simple_token.chars().count()));
+            }
+        }
+
+        Some(Err(format!("Unrecognized symbol: {}", self.get_current_char())))
     }
 }
