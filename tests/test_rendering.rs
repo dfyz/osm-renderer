89aa6ded8745fd53732c1d799d0dce296d9c4313
Ivan Komarov <Ivan.Komarov@dfyz.info>
2018-02-04 06:25:05 +0300

    fix a race condition in tests

--- tests/test_rendering.rs
+++ tests/test_rendering.rs
@@ -83,7 +83,7 @@ fn compare_png_outputs(zoom: u8) {
 }
 
 fn test_rendering_zoom(zoom: u8, min_x: u32, max_x: u32, min_y: u32, max_y: u32) {
-    let bin_file = common::get_test_path(&["osm", "nano_moscow.bin"]);
+    let bin_file = common::get_test_path(&["osm", &format!("nano_moscow_{}.bin", zoom)]);
     renderer::geodata::importer::import(
         &common::get_test_path(&["osm", "nano_moscow.osm"]),
         &bin_file,
