810633107efb4e7454e761eab6ad499a01db1882
Ivan Komarov <Ivan.Komarov@dfyz.info>
2018-06-29 23:26:14 +0300

    read configuration from config files

--- src/bin/renderer.rs
+++ src/bin/renderer.rs
@@ -1,37 +1,78 @@
 extern crate error_chain;
+extern crate ini;
 extern crate renderer;
 
 use error_chain::ChainedError;
+use ini::ini::Properties;
+use ini::Ini;
 use renderer::http_server::run_server;
 use renderer::mapcss::styler::StyleType;
 use std::env;
 
+fn fail() -> ! {
+    std::process::exit(1);
+}
+
+type NamedSection<'a, 'b> = (&'a Properties, &'b str);
+
+fn get_section_from_config<'a, 'b>(config: &'a Ini, section_name: &'b str) -> NamedSection<'a, 'b> {
+    match config.section(Some(section_name)) {
+        Some(section) => (section, section_name),
+        _ => {
+            eprintln!("The [{}] section is missing", section_name);
+            fail();
+        }
+    }
+}
+
+fn get_value_from_config<'a>(section: &'a NamedSection, name: &str) -> &'a str {
+    match section.0.get(name) {
+        Some(value) => value,
+        _ => {
+            eprintln!("Property {} is missing in section [{}]", name, section.1);
+            fail();
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() < 5 {
+    if args.len() < 2 {
         let bin_name = args.first().map(|x| x.as_str()).unwrap_or("renderer");
-        eprintln!(
-            "Usage: {} SERVER_ADDRESS GEODATA_FILE STYLESHEET_FILE STYLESHEET_TYPE [OSM_IDS]",
-            bin_name
-        );
-        std::process::exit(1);
+        eprintln!("Usage: {} CONFIG [OSM_IDS]", bin_name);
+        fail();
     }
 
-    let server_address = &args[1];
-    let geodata_file = &args[2];
-    let stylesheet_file = &args[3];
-    let stylesheet_type = match args[4].as_str() {
+    let config_path = &args[1];
+    let config = match Ini::load_from_file(config_path) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Failed to parse config from {}: {}", config_path, err);
+            fail();
+        }
+    };
+
+    let http_section = get_section_from_config(&config, "http");
+    let server_address = get_value_from_config(&http_section, "address");
+
+    let geodata_section = get_section_from_config(&config, "geodata");
+    let geodata_file = get_value_from_config(&geodata_section, "file");
+
+    let style_section = get_section_from_config(&config, "style");
+    let stylesheet_file = get_value_from_config(&style_section, "file");
+    let stylesheet_type = match get_value_from_config(&style_section, "type") {
         "josm" => StyleType::Josm,
         "mapsme" => StyleType::MapsMe,
         unknown_style => {
             eprintln!("Unknown stylesheet type: {}", unknown_style);
-            std::process::exit(1);
+            fail();
         }
     };
-    let osm_ids = if args.len() >= 6 {
+
+    let osm_ids = if args.len() >= 3 {
         Some(
-            args[5..]
+            args[2..]
                 .iter()
                 .map(|x| x.parse().unwrap_or_else(|_| panic!("Invalid OSM ID: {}", x)))
                 .collect(),
@@ -44,6 +85,6 @@ fn main() {
 
     if let Err(e) = res {
         eprintln!("{}", e.display_chain());
-        std::process::exit(1);
+        fail();
     }
 }
