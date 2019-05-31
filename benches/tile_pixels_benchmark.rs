58ad805bd9e9d65e5a644b53d7d40f06a478234c
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-06-01 02:20:31 +0300

    cargo fmt

--- benches/tile_pixels_benchmark.rs
+++ benches/tile_pixels_benchmark.rs
@@ -2,11 +2,15 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use renderer::draw::tile_pixels::TilePixels;
 
 fn tile_pixels_benchmark_1(c: &mut Criterion) {
-    c.bench_function("create TilePixels, scale = 1", |b| b.iter(|| TilePixels::new(black_box(1))));
+    c.bench_function("create TilePixels, scale = 1", |b| {
+        b.iter(|| TilePixels::new(black_box(1)))
+    });
 }
 
 fn tile_pixels_benchmark_2(c: &mut Criterion) {
-    c.bench_function("create TilePixels, scale = 2", |b| b.iter(|| TilePixels::new(black_box(2))));
+    c.bench_function("create TilePixels, scale = 2", |b| {
+        b.iter(|| TilePixels::new(black_box(2)))
+    });
 }
 
 criterion_group!(tile_pixels_benches, tile_pixels_benchmark_1, tile_pixels_benchmark_2);
