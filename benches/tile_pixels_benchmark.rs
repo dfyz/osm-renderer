01535b564a2c2e5f539187d2bc56872d52b0f265
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-06-05 03:15:51 +0300

    cargo fmt

--- benches/tile_pixels_benchmark.rs
+++ benches/tile_pixels_benchmark.rs
@@ -4,21 +4,31 @@ use renderer::mapcss::color::Color;
 
 fn tile_pixels_benchmark_1(c: &mut Criterion) {
     c.bench_function("create TilePixels, scale = 1", |b| {
-        b.iter(|| TilePixels::new(black_box(1), &Some(Color {
-            r: 0xe3,
-            g: 0xe1,
-            b: 0xd2,
-        })))
+        b.iter(|| {
+            TilePixels::new(
+                black_box(1),
+                &Some(Color {
+                    r: 0xe3,
+                    g: 0xe1,
+                    b: 0xd2,
+                }),
+            )
+        })
     });
 }
 
 fn tile_pixels_benchmark_2(c: &mut Criterion) {
     c.bench_function("create TilePixels, scale = 2", |b| {
-        b.iter(|| TilePixels::new(black_box(2), &Some(Color {
-            r: 0xe3,
-            g: 0xe1,
-            b: 0xd2,
-        })))
+        b.iter(|| {
+            TilePixels::new(
+                black_box(2),
+                &Some(Color {
+                    r: 0xe3,
+                    g: 0xe1,
+                    b: 0xd2,
+                }),
+            )
+        })
     });
 }
 
