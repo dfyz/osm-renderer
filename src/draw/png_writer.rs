054f694cb2d0594c9d1469e3cd94cabf83ff8d70
Ivan Komarov <Ivan.Komarov@dfyz.info>
2020-03-02 03:01:20 +0300

    Update deps and slightly modify the PNG writing code after API changes in the `png` crate

--- src/draw/png_writer.rs
+++ src/draw/png_writer.rs
@@ -1,11 +1,11 @@
 use failure::{Error, ResultExt};
-use png::{ColorType, Encoder, HasParameters};
+use png::{ColorType, Encoder};
 
 pub fn rgb_triples_to_png(triples: &[(u8, u8, u8)], width: usize, height: usize) -> Result<Vec<u8>, Error> {
     let mut buf = Vec::new();
     {
         let mut png_encoder = Encoder::new(&mut buf, width as u32, height as u32);
-        png_encoder.set(ColorType::RGB);
+        png_encoder.set_color(ColorType::RGB);
         let mut png_writer = png_encoder.write_header().context("Failed to write PNG header")?;
 
         let mut image_bytes = Vec::new();
